02082751ab31d2d710abde2899567babfd9799cc
rivaldofez <rivaldofez@gmail.com>
2022-04-23 03:41:40 +0700

    Initial Commit

---
+++ ColorPalette.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Wawai
+//
+//  Created by Rivaldo Fernandes on 23/04/22.
+//
+
+import SwiftUI
+
+
+struct ColorPalette {
+    static let khaki = Color(red: 255/255, green: 203/255, blue: 116/255)
+    static let deepgray = Color(red: 46/255, green: 46/255, blue: 46/255)
+    static let whitesmoke = Color(red: 246/255, green: 246/255, blue: 246/255)
+    static let dimgray = Color(red: 111/255, green: 111/255, blue: 111/255)
+    static let lightgray = Color(red: 215/255, green: 214/255, blue: 214/255)
+    static let choco = Color(red: 152/255, green: 127/255, blue: 85/255)
+}
