0568792686a59f6bbdbb7700b64fbd132b06d2d3
rivaldofez <rivaldofez@gmail.com>
2022-04-23 04:30:47 +0700

    create page sub character

---
+++ SubCharacterView.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  Wawai
+//
+//  Created by Rivaldo Fernandes on 23/04/22.
+//
+
+import SwiftUI
+
+
+struct SubCharacterView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+struct SubCharacterView_Previews: PreviewProvider {
+    static var previews: some View {
+        SubCharacterView()
+    }
+}
