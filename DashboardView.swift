cb62c6c5ab32765dbf57120c320693b7c2f4523b
rivaldofez <rivaldofez@gmail.com>
2022-04-24 20:15:29 +0700

    create dashboardview

---
+++ DashboardView.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  Wawai
+//
+//  Created by Rivaldo Fernandes on 24/04/22.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+struct DashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardView()
+    }
+}
